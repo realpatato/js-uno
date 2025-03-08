31c6d86d2bf08818de140f9693b16d280a1c2b3d
Jason <jason.t.richardii@gmail.com>
2025-03-08 09:13:21 -0500

    added application (canvas in html) to js file, draws a square too.

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+import {Application, Graphics} from "pixi.js";
+
 (async() => {
-    //entire application goes here
+    const app = new Application();
+    await app.init({
+        resizeTo: window,
+        backgroundAlpha: 0.5
+    });
+    app.canvas.style.position = 'absolute'
+    document.body.appendChild(app.canvas)
+
+    const rectangle = new Graphics()
+        .rect(200, 200, 200, 180)
+        .fill({
+            color: 0xffea00,
+            alpha: 0.5
+        })
+        .stroke({
+            width: 8,
+            color: 0x00ff00
+        });
+    app.stage.addChild(rectangle);
 })();

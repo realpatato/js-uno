20c579966222e16d2d82cc0605c2729233ae3da3
Jason <jason.t.richardii@gmail.com>
2025-03-10 18:10:59 -0400

    added a sprite to the screen

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {Application, Graphics} from "pixi.js";
+import {Application, Graphics, Assets, Sprite} from "pixi.js";
 
 (async() => {
     const app = new Application();
@@ -20,4 +20,10 @@ import {Application, Graphics} from "pixi.js";
             color: 0x00ff00
         });
     app.stage.addChild(rectangle);
+    
+    const texture = await Assets.load('spritesheet-data/spritesheet-images/backside.png');
+    const sprite = new Sprite(texture);
+    texture.source.scaleMode = 'nearest';
+    sprite.scale.set(10);
+    app.stage.addChild(sprite);
 })();

63131842feb8ab54f6bc03d58d5a372a43ddb57e
realpatato <jason.t.richardii@gmail.com>
2025-04-16 14:16:22 -0400

    added image to canvas

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 //EMPTY
+const ctx = document.getElementById("canvas").getContext("2d");
+const img = new Image();
+
+img.addEventListener("load", () => {
+  ctx.drawImage(img, 0, 0);
+});
+
+img.src = "spritesheet-images/yellow-cards.png";
